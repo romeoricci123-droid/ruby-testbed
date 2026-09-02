b0b373fd43660088c63f1b4a20621e2c6668e312
MAESTRO <maestro@local>
2026-09-02 10:03:51 +0200

    [maestro] Add an uppercase option to the greeting function in lib/greeting.rb so i
    Mission: 00b2d99a0be2
    Scores: reviewer=100

--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe "greeting" do
   it "has the expected version" do
     expect(VERSION).to eq("0.1.0")
   end
+
+  it "returns the greeting in all caps when uppercase is true" do
+    expect(greeting(uppercase: true)).to eq("HELLO FROM RUBY-TESTBED")
+  end
 end
